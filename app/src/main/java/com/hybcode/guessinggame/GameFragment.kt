365dfe5c7785c0538e28ded837a9631a7ddef254
rawaha-coder <rawaha.coder@gmail.com>
2022-02-03 19:30:50 +0100

    This commit is after testing the app work with layout view and before start migrating to Compose UI

--- app/src/main/java/com/hybcode/guessinggame/GameFragment.kt
+++ app/src/main/java/com/hybcode/guessinggame/GameFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hybcode.guessinggame.databinding.FragmentGameBinding
 import androidx.navigation.findNavController
@@ -32,18 +30,6 @@ class GameFragment : Fragment() {
         binding.gameViewModel = viewModel
 
         binding.lifecycleOwner = viewLifecycleOwner
-
-//        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { guess ->
-//            binding.incorrectGuesses.text = getString(R.string.incorrect_guesses, guess)
-//        })
-//
-//        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { lives ->
-//            binding.lives.text = getString(R.string.lives_left, lives)
-//        })
-//
-//        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { secretWord ->
-//            binding.word.text = secretWord
-//        })
 
         viewModel.gameOver.observe(viewLifecycleOwner, Observer {
             if (it) {
